055280d0c2eebb138b39f70f9f70f1f4da9a77a3
the-Astra <djs8743@rit.edu>
2026-09-07 15:26:18 -0400

    get pool hook is too oppressive

--- src/overrides.lua
+++ src/overrides.lua
@@ -2,44 +2,45 @@
 local gcp = get_current_pool
 function get_current_pool(_type, _rarity, _legendary, _append)
     local _pool, _pool_key = gcp(_type, _rarity, _legendary, _append)
-    local _pool_size = 0
+    if G.STAGE == G.STAGES.RUN then
+        local _pool_size = 0
 
-    for i = 1, #_pool do
-        if _pool[i] ~= 'UNAVAILABLE' then
-            local key = _pool[i]
+        for i = 1, #_pool do
+            if _pool[i] ~= 'UNAVAILABLE' then
+                local key = _pool[i]
 
-            if G.P_CENTERS[key] and not SuperRogue.is_object_mod_active(G.P_CENTERS[key], {type = _type}) then
-                _pool[i] = 'UNAVAILABLE'
-            elseif G.P_SEALS[key] and not SuperRogue.is_object_mod_active(G.P_SEALS[key], {type = _type}) then
-                _pool[i] = 'UNAVAILABLE'
-            elseif G.P_TAGS[key] and not SuperRogue.is_object_mod_active(G.P_TAGS[key], {type = _type}) then
-                _pool[i] = 'UNAVAILABLE'
-            else
-                _pool_size = _pool_size + 1
+                if G.P_CENTERS[key] and not SuperRogue.is_object_mod_active(G.P_CENTERS[key], {type = _type}) then
+                    _pool[i] = 'UNAVAILABLE'
+                elseif G.P_SEALS[key] and not SuperRogue.is_object_mod_active(G.P_SEALS[key], {type = _type}) then
+                    _pool[i] = 'UNAVAILABLE'
+                elseif G.P_TAGS[key] and not SuperRogue.is_object_mod_active(G.P_TAGS[key], {type = _type}) then
+                    _pool[i] = 'UNAVAILABLE'
+                else
+                    _pool_size = _pool_size + 1
+                end
+            end
+        end
+
+        --if pool is empty
+        if _pool_size == 0 then
+            _pool = EMPTY(G.ARGS.TEMP_POOL)
+            if SMODS.ObjectTypes[_type] and SMODS.ObjectTypes[_type].default and G.P_CENTERS[SMODS.ObjectTypes[_type].default] then
+                if SuperRogue.is_object_mod_active(SMODS.ObjectTypes[_type]) then
+                    _pool[#_pool+1] = SMODS.ObjectTypes[_type].default
+                end
+            elseif _type == 'Tarot' or _type == 'Tarot_Planet' then _pool[#_pool + 1] = "c_strength"
+            elseif _type == 'Planet' then _pool[#_pool + 1] = "c_pluto"
+            elseif _type == 'Spectral' then _pool[#_pool + 1] = "c_incantation"
+            elseif _type == 'Joker' then _pool[#_pool + 1] = "j_joker"
+            elseif _type == 'Demo' then _pool[#_pool + 1] = "j_joker"
+            elseif _type == 'Voucher' then _pool[#_pool + 1] = "v_blank"
+            elseif _type == 'Tag' then _pool[#_pool + 1] = "tag_handy"
+            elseif _type == 'Edition' then _pool[#_pool + 1] = "e_foil"
+            elseif _type == 'Seal' then _pool[#_pool + 1] = "Purple"
+            else _pool[#_pool + 1] = "j_joker"
             end
         end
     end
-
-    --if pool is empty
-    if _pool_size == 0 then
-        _pool = EMPTY(G.ARGS.TEMP_POOL)
-        if SMODS.ObjectTypes[_type] and SMODS.ObjectTypes[_type].default and G.P_CENTERS[SMODS.ObjectTypes[_type].default] then
-            if SuperRogue.is_object_mod_active(SMODS.ObjectTypes[_type]) then
-                _pool[#_pool+1] = SMODS.ObjectTypes[_type].default
-            end
-        elseif _type == 'Tarot' or _type == 'Tarot_Planet' then _pool[#_pool + 1] = "c_strength"
-        elseif _type == 'Planet' then _pool[#_pool + 1] = "c_pluto"
-        elseif _type == 'Spectral' then _pool[#_pool + 1] = "c_incantation"
-        elseif _type == 'Joker' then _pool[#_pool + 1] = "j_joker"
-        elseif _type == 'Demo' then _pool[#_pool + 1] = "j_joker"
-        elseif _type == 'Voucher' then _pool[#_pool + 1] = "v_blank"
-        elseif _type == 'Tag' then _pool[#_pool + 1] = "tag_handy"
-        elseif _type == 'Edition' then _pool[#_pool + 1] = "e_foil"
-        elseif _type == 'Seal' then _pool[#_pool + 1] = "Purple"
-        else _pool[#_pool + 1] = "j_joker"
-        end
-    end
-
     return _pool, _pool_key
 end
 
