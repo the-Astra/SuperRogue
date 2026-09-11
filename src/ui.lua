6ed4c907681e27a415d66f5b0678b2c224503d57
the-Astra <djs8743@rit.edu>
2026-09-11 18:45:19 -0400

    Fix incorrect strings in run info

--- src/ui.lua
+++ src/ui.lua
@@ -102,14 +102,14 @@ function G.UIDEF.sr_activated_mods()
         { n = G.UIT.R, config = { align = "cm", padding = 0, no_fill = true }, nodes = mod_tables }
     )
 
-    local tracking_string = localize('k_sr_no_mods_left')
+    local tracking_string = localize('ph_sr_no_mods_left')
     local steps_to_activation = G.GAME.sr_activation_threashold - G.GAME.sr_iteration_steps
     if SuperRogue.get_total_inactive() == 0 then
         -- Don't change the tracking string
-    elseif G.GAME.sr_activation_mode == 1 then
-        tracking_string = localize{type = 'variable', key = 'k_sr_rounds_until_next_mod', vars = {steps_to_activation}}
-    elseif G.GAME.sr_activation_mode == 2 then
+    elseif G.GAME.sr_trigger_type == 1 then
         tracking_string = localize{type = 'variable', key = 'k_sr_antes_until_next_mod', vars = {steps_to_activation}}
+    elseif G.GAME.sr_trigger_type == 2 then
+        tracking_string = localize{type = 'variable', key = 'k_sr_rounds_until_next_mod', vars = {steps_to_activation}}
     end
 
     local t = silent and {
